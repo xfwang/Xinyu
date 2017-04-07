485376be0e1bc7ed128cf376d62cd5c7a2a61640
Xinyu Tian <sarahtxy@gmail.com>
2017-04-06 22:32:05 -0700

    Add files via upload

--- functions/result_process.R
+++ functions/result_process.R
@@ -1,3 +1,5 @@
+library(dplyr);library(rstan)
+
 #####---------------------------------------------------------------------------------------------------------#####
 ##### R script to shrink parameters using 95% CI calculated in stan()                                         #####
 #####                                                                                                         #####
@@ -7,16 +9,22 @@
 ##### Output:                                                                                                 #####
 #####       idx <= 0          :=  a vector of indicators on if a parameter is 0 or not                        #####
 #####---------------------------------------------------------------------------------------------------------#####
-shrink.stan <- function(stan.ext, confidence.level = 0.95) {
+shrink_ind.stan <- function(stan.ext, confidence.level = 0.95) {
   idx <- stan.ext %>% 
     apply(2, quantile, c((1 - confidence.level) / 2, (1 + confidence.level) / 2)) %>%
     apply(2, prod)
   return((idx <= 0))
 }
 
-param.stan <- function(stan.obj, param = "beta", confidence.level = 0.95) {
+shrink.stan <- function(stan.obj, param = "beta", confidence.level = 0.95) {
+  beta_all <- param.stan(stan.obj, param = param)
+  beta <- colMeans(beta_all) * as.numeric(!shrink_ind.stan(beta_all, confidence.level = confidence.level))
+  return(beta)
+}
+
+param.stan <- function(stan.obj, param = "beta") {
   la <- rstan::extract(stan.obj, permuted = T)
-  beta <- colMeans(la[[param]]) * as.numeric(!shrink.stan(la[[param]], confidence.level = confidence.level))
+  beta <- la[[param]]
   return(beta)
 }
 
@@ -34,3 +42,27 @@ stan_plot <- function(stan.list, type = c("Rhat")) {
   return(pp)
 }
 
+#####---------------------------------------------------------------------------------------------------------#####
+##### R script to find out the rank of shrinkage in the coefficients in stan                                  #####
+##### ASSUMPTION: normal distribution of beta                                                                 #####
+#####                                                                                                         #####
+##### Input:                                                                                                  #####
+#####       beta.list         :=  the series of sampled posterior coefficients for one parameter              #####
+#####       beta.array        :=  the array of sampled posterior coefficients for several parameters (niter*P)#####
+##### Output:                                                                                                 #####
+#####       credible_level    :=  how likely the true parameter is 0, which is the cumulative probability of 0#####
+#####---------------------------------------------------------------------------------------------------------#####
+cred_prob <- function(beta.list) {
+  cum_prob <- pnorm(0, mean = mean(beta.list), sd = sd(beta.list))
+  return(min(cum_prob, 1-cum_prob)*2)
+}
+
+cred_quantile <- function(beta.list) {
+  cut_perc <- mean(beta.list < 0)
+  return(min(cut_perc, 1-cut_perc)*2)
+}
+
+rank_select <- function(stan.obj, param = "beta") {
+  beta.array <- param.stan(stan.obj, param = param)
+  return(apply(beta.array, 2, cred_quantile))
+}
